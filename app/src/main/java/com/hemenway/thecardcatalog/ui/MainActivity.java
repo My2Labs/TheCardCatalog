1c39b0492b9980234b1d28b431a7acdd9dfe547f
Sharron H <sharronhemenway@outlook.com>
2023-12-18 18:57:49 -0600

    Modify MainActivity.java.

--- app/src/main/java/com/hemenway/thecardcatalog/ui/MainActivity.java
+++ app/src/main/java/com/hemenway/thecardcatalog/ui/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.hemenway.thecardcatalog.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
